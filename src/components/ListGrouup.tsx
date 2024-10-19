aedccf269c3fd56de71d37a6b82980a4a97d263d
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-10-19 07:55:26 +0530

    consitional rendeering

--- src/components/ListGrouup.tsx
+++ src/components/ListGrouup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "San Franscisco", "Tokyo", "London", "paris"];
+  let items = ["New York", "San Franscisco", "Tokyo", "London", "paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
