29b0b98ff4c9cff95336a3e6388fd9494cc6f870
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-10-20 06:33:08 +0530

    3.6 handling Event

--- src/components/ListGrouup.tsx
+++ src/components/ListGrouup.tsx
@@ -1,17 +1,20 @@
 function ListGroup() {
   let items = ["New York", "San Franscisco", "Tokyo", "London", "paris"];
-  items = [];
+  const handleClick = (event: React.MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
 }
+// 3.6 handling Events
 export default ListGroup;
