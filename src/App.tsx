1628cd7358db6aa9aea01833bcd0ddcdc133d09e
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-10-20 08:29:25 +0530

    3.8 Passing the props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ListGroup from "./components/ListGrouup";
 
+let items = ["New York", "San Franscisco", "Tokyo", "London", "paris"];
 function App() {
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
