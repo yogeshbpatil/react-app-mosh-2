cb9593f592e586d31c9f5d6d352a9bbfb78a1533
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-10-20 09:17:52 +0530

    3.9 passing function from props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import ListGroup from "./components/ListGrouup";
 
 let items = ["New York", "San Franscisco", "Tokyo", "London", "paris"];
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
+
 function App() {
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGrouup.tsx
+++ src/components/ListGrouup.tsx
@@ -3,8 +3,9 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -14,14 +15,16 @@ function ListGroup({ items, heading }: Props) {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            // list-group-item  active
             className={
               selectedIndex === index
                 ? "list-group-item  active"
                 : "list-group-item "
             }
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
